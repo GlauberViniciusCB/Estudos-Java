6cef241e65d98a8b6de064c259d3867797ba4684
GlauberViniciusCB <g_viniciuscb@outlook.com>
2025-08-20 16:55:47 -0300

    Iniciando Exercicios Basicos Condicionais

---
+++ Java/Estrutura_Sequencial_Java/src/Estrutura_Condicional/Exercicio_1.java
@@ -0,0 +1,7 @@
+package Estrutura_Condicional;
+/*Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não. */ 
+public class Exercicio_1 {
+    public static void main(String[] args) {
+        
+    }
+}
