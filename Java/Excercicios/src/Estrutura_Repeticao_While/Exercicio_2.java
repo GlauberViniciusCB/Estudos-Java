9c26ac83358041a55341465f05561031332030f2
GlauberViniciusCB <g_viniciuscb@outlook.com>
2025-08-27 22:50:17 -0300

    Finalizando Exercicios de while

--- Java/Excercicios/src/Estrutura_Repeticao_While/Exercicio_2.java
+++ Java/Excercicios/src/Estrutura_Repeticao_While/Exercicio_2.java
@@ -10,6 +10,37 @@ public class Exercicio_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        double coordenadaX,coodernadaY;
 
+        System.out.println("Informe A Coordenada X: ");
+        coordenadaX = input.nextInt();
+
+        System.out.println("Informe A Coordenada Y: ");
+        coodernadaY = input.nextInt();
+
+        while (coordenadaX !=0 || coodernadaY != 0 ) {
+            if (coordenadaX > 0  && coodernadaY > 0) {
+                System.out.println("Primeiro");
+            }
+            else if (coordenadaX < 0 && coodernadaY > 0){
+                System.out.println("Segundo");
+            }
+            else if (coordenadaX < 0 && coodernadaY < 0 ){
+                System.out.println("Terceiro");
+            }
+            else if (coordenadaX > 0 && coodernadaY < 0){
+                System.out.println("Quarto");
+            }
+            else{
+                break;
+            }
+
+            System.out.println("Informe A Coordenada X: ");
+            coordenadaX = input.nextInt();
+
+            System.out.println("Informe A Coordenada Y: ");
+            coodernadaY = input.nextInt();
+        }
+    
     }
 }
