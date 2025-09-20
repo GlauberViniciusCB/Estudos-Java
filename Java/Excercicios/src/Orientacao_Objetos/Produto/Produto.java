7bf6d0b28acafdcfd490f1bf40d1a4c04931006b
GlauberViniciusCB <g_viniciuscb@outlook.com>
2025-09-20 16:05:01 -0300

    Resolvendo Ex 2

---
+++ Java/Excercicios/src/Orientacao_Objetos/Produto/Produto.java
@@ -0,0 +1,11 @@
+package Orientacao_Objetos.Produto;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public double TotalNoEstoque(){
+        
+    }
+}
