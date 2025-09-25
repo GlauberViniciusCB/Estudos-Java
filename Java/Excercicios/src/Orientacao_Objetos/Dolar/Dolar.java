f3fb688afe055f4210ed1e8ab65b96c2d878927e
GlauberViniciusCB <g_viniciuscb@outlook.com>
2025-09-24 22:10:09 -0300

    Resolvendo Questão Do Dolar

---
+++ Java/Excercicios/src/Orientacao_Objetos/Dolar/Dolar.java
@@ -0,0 +1,6 @@
+package Orientacao_Objetos.Dolar;
+
+public class Dolar {
+    public static final double TAXA_DOLAR = 0.06 ;
+    
+}
---
+++ Java/Excercicios/src/Orientacao_Objetos/Dolar/dolar_Main.java
@@ -0,0 +1,17 @@
+package Orientacao_Objetos.Dolar;
+
+import java.util.Scanner;
+
+public class dolar_Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe O Valor De Cotação Do Dolar: ");
+        double valorDeContacaoDolar = input.nextDouble();
+        
+
+
+    }
+
+
+}
