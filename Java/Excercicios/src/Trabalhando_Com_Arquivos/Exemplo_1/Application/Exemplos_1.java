eb42d126a88bc602cf614e07e91d1212800b0eff
GlauberViniciusCB <g_viniciuscb@outlook.com>
2026-04-19 13:44:41 -0300

    Resolução Exercício 1 - Arquivos

---
+++ Java/Excercicios/src/Trabalhando_Com_Arquivos/Exemplo_1/Application/Exemplos_1.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exemplos_1 {
+    public static void main(String[] args) {
+        
+        File arquivo = new File("C:\\Users\\Glauber Vinicius\\Documents\\Java\\Java\\Excercicios\\src\\Trabalhando_Com_Arquivos\\Exemplo_1\\Application\\ArquivoTeste.txt");
+        Scanner input = null;
+
+        try {
+            input = new Scanner(arquivo);
+            while (input.hasNextLine()) {
+                System.out.println(input.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+        finally{
+            if(input!= null){
+                input.close();
+            }
+        }
+        
+    }
+}
